b0add03e7e730ba2de682782b26ce5be7869cf90
Davyd Semytotskyi <davidsemitotskiy@gmail.com>
2024-05-26 19:28:23 +0300

    Added DbContext and EntityType configurations.

---
+++ UrlShortener/UrlShortener.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Models;
+
+namespace UrlShortener.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+
+        public DbSet<ShortUrl> ShortUrls { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ UrlShortener/UrlShortener.Infrastructure/Persistence/Configurations/ShortUrlEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UrlShortener.Domain.Models;
+
+namespace UrlShortener.Infrastructure.Persistence.Configurations
+{
+    public class ShortUrlEntityTypeConfiguration : IEntityTypeConfiguration<ShortUrl>
+    {
+        public void Configure(EntityTypeBuilder<ShortUrl> builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+
+            builder
+                .HasOne(shortUrl => shortUrl.User)
+                .WithMany(user => user.ShortUrls)
+                .HasForeignKey(shortUrl => shortUrl.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
